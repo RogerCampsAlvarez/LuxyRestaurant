feabd2d2573dd3c7f14f88daab34c4cb4a0d2ef8
Abelatox <abel54225@gmail.com>
2018-05-02 09:48:31 +0200

    Les taules es carreguen de la BD

--- src/application/admin/TaulesController.java
+++ src/application/admin/TaulesController.java
@@ -1,7 +1,10 @@
 package application.admin;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import application.ConnexioBD;
 import application.Strings;
 import application.Util;
 import javafx.collections.FXCollections;
@@ -21,13 +24,15 @@ public class TaulesController {
 
 	@FXML
 	private Button btnBack;
-
 	@FXML
 	private TableView<Taula> taulesTableView;
 	@FXML
 	private TableColumn<Taula, String> tcNom;
 	@FXML
 	private TableColumn<Taula, String> tcCapacitat;
+	
+	private ConnexioBD con;
+	ObservableList<Taula> olTaulesList = FXCollections.observableArrayList();
 
 	@FXML
 	void btnBack(ActionEvent event) throws IOException {
@@ -38,19 +43,23 @@ public class TaulesController {
 	}
 
 	public void initialize() {
+		con = new ConnexioBD();
 		loadTaules();
 	}
 
 	private void loadTaules() {
 		// TODO Load from DB
 
-		Taula t1 = new Taula("Taula 1", 10);
-		Taula t2 = new Taula("Taula 2", 6);
-
-		ObservableList<Taula> olTaulesList = FXCollections.observableArrayList();
-
-		olTaulesList.add(t1);
-		olTaulesList.add(t2);
+		try {
+			ResultSet rs = con.queryDB("select * from taules order by id asc");
+			
+			olTaulesList.clear();
+			while (rs.next()) { // Carrega els productes per categoria
+				olTaulesList.add(new Taula(/*rs.getInt("id")+*/ rs.getString("nom"), rs.getInt("persones")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		//Linies necessaries per poder inserir valors a les cel·les
 		tcNom.setCellValueFactory(new PropertyValueFactory<Taula, String>("nom"));
